bf48ecd6b37894bb0551a2ca927a4e5c4776921b
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-04-09 12:50:51 +0200

    added @With to timePoint class

--- src/main/java/pl/zajavka/mortgage/model/TimePoint.java
+++ src/main/java/pl/zajavka/mortgage/model/TimePoint.java
@@ -1,11 +1,13 @@
 package pl.zajavka.mortgage.model;
 
 import lombok.Value;
+import lombok.With;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Value
+@With
 public class TimePoint {
 
     BigDecimal year;
