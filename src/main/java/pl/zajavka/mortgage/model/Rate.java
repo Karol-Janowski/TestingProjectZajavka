773f09b88ccdc4883d6c5d7233c2e456b191d09d
Karol Janowski <104912717+Karol-Janowski@users.noreply.github.com>
2024-04-09 12:47:59 +0200

    added @buider @With @AllArgsConstructor to Rate class

--- src/main/java/pl/zajavka/mortgage/model/Rate.java
+++ src/main/java/pl/zajavka/mortgage/model/Rate.java
@@ -1,10 +1,16 @@
 package pl.zajavka.mortgage.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Value;
+import lombok.With;
 
 import java.math.BigDecimal;
 
 @Value
+@Builder
+@With
+@AllArgsConstructor
 public class Rate {
 
     BigDecimal rateNumber;
